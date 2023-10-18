815618b9ec6e0896b0ce51718724d55244b48ee7
Ivan Aguilar <ivan@MacBook-Pro-de-Ivan.local>
2023-10-18 21:43:10 +0200

    Type Advisor class

---
+++ src/Advisor/Advisor.ts
@@ -0,0 +1,15 @@
+import Character from "../Character/Character";
+import type { AdvisorData } from "../types/types";
+
+export class Advisor extends Character {
+  adviseCharacter;
+
+  constructor(data: AdvisorData) {
+    super(data);
+    this.adviseCharacter = data.adviseCharacter;
+  }
+
+  communicate(): string {
+    return "No sé por qué, pero creo que voy a morir pronto";
+  }
+}
