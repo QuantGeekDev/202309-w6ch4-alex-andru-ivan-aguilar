34227ffbfbf2a0ea3c62aee157d871bbaf53c5da
Ivan Aguilar <ivan@MacBook-Pro-de-Ivan.local>
2023-10-19 21:38:47 +0200

    Add CharactersCards class

---
+++ src/Components/CharactersCards/CharactersCards.ts
@@ -0,0 +1,9 @@
+import Component from "../component/component.js";
+
+class CharactersCards extends Component {
+  protected populate(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export default CharactersCards;
