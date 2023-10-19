0c5f1b5231ff30e3618b387e9755d7f1bed404e0
Ivan Aguilar <ivan@MBP-de-Ivan.technicolor.net>
2023-10-19 02:39:40 +0200

    Add abstract class Component

---
+++ src/component/component.ts
@@ -0,0 +1,24 @@
+
+abstract class Component {
+  protected readonly element: Element;
+  protected readonly parentElement: Element;
+
+  constructor(parentElemenent: Element, tagName: string, className: string) {
+    this.parentElement = parentElemenent;
+    this.element = document.createElement(tagName);
+    this.element.className = className;
+  }
+
+  public render(){
+    if (this.parentElement === undefined) {
+      throw Error("there is no parentElemen")
+    }
+
+    this.parentElement.appendChild(this.element);
+    this.populate();
+  }
+
+  protected abstract populate() void;
+}
+
+export default Component;
